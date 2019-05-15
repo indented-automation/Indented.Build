8027e8dc149530d47bf65684908166b64fbd0224
Chris Dent <chris@indented.co.uk>
2019-05-15 11:48:49 +0100

    Fixes nunit file path

--- Indented.Build/task/Test/UploadAppVeyorTestResults.ps1
+++ Indented.Build/task/Test/UploadAppVeyorTestResults.ps1
@@ -3,7 +3,7 @@
 } -Definition {
     # Upload any test results to AppVeyor.
 
-    $path = Join-Path $buildInfo.Path.Build.Output ('{0}.xml' -f $buildInfo.ModuleName)
+    $path = Join-Path $buildInfo.Path.Build.Output ('{0}-nunit.xml' -f $buildInfo.ModuleName)
     if (Test-Path $path) {
         [System.Net.WebClient]::new().UploadFile(('https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID), $path)
     }
